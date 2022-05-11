6c9d38d70684551f822e35cc75bff6de82b2f6be
Miroslavetsh <dfgshte@gmail.com>
2022-05-11 16:23:17 +0300

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+function greet(name) {
+    const greeting = document.createElement("h1");
+    greeting.textContent = `Hello ${name}`;
+    document.body.appendChild(greeting);
+}
+greet("Typescript");
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6Ii4uL3NyYy8iLCJzb3VyY2VzIjpbImFwcC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsU0FBUyxLQUFLLENBQUMsSUFBWTtJQUN6QixNQUFNLFFBQVEsR0FBRyxRQUFRLENBQUMsYUFBYSxDQUFDLElBQUksQ0FBQyxDQUFBO0lBQzdDLFFBQVEsQ0FBQyxXQUFXLEdBQUcsU0FBUyxJQUFJLEVBQUUsQ0FBQTtJQUN0QyxRQUFRLENBQUMsSUFBSSxDQUFDLFdBQVcsQ0FBQyxRQUFRLENBQUMsQ0FBQTtBQUNyQyxDQUFDO0FBRUQsS0FBSyxDQUFDLFlBQVksQ0FBQyxDQUFBIn0=
